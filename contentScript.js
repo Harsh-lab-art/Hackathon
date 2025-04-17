fcbfb10b17f8c41c8f6c9d89a4ea86b816116902
Harsh Kumar Verma <harsh9760verma@gmail.com>
2025-04-17 20:51:26 +0530

    contentScript.js

---
+++ contentScript.js
@@ -0,0 +1,47 @@
+(() => {
+    let mailInboxTable, mailContent;
+    let currentMail = "";
+    let currentMailAnchors = [];
+    chrome.runtime.onMessage.addListener((obj, sender, response) => {
+        const {type, value, mailId} = obj;
+
+        if (type === "NEW") {
+            currentMail = mailId;
+            newMailLoaded();
+        }
+    });
+
+    const newMailLoaded = () => {
+        const bookmarkBtnExists = document.querySelector(".bookmark-btn");
+
+        if (!bookmarkBtnExists) {
+            const bookmarkBtn = document.createElement("img");
+
+            bookmarkBtn.src = chrome.runtime.getURL("assets/bookmark.png");
+            bookmarkBtn.className = "T-KT " + "bookmark-btn";
+            bookmarkBtn.title = "Click to scan for phising links";
+
+            mailInboxTable = document.querySelector('.gE.iv.gt')
+            mailInboxTable.append(bookmarkBtn);
+            bookmarkBtn.addEventListener("click", addNewBookmarkEventHandler);
+        }
+    }
+
+    const addNewBookmarkEventHandler = async () => {
+        mailContent = document.querySelector('.gs')
+        let linksArray = mailContent.querySelectorAll("a");
+        for (let link of linksArray) {
+            currentMailAnchors.push(link.href)
+        }
+        chrome.storage.local.set({
+            [currentMail]: JSON.stringify([...currentMailAnchors])
+        }).then(data => {
+        });
+        chrome.storage.local.get([currentMail]).then(data => {
+            console.log(data)
+        })
+        currentMailAnchors = [];
+    }
+
+    newMailLoaded()
+})();
